af1e557880c38061416e4ca98b1d30653f72b340
hs673 <90913830+hs673@users.noreply.github.com>
2023-09-16 23:30:57 -0400

    Delete hwa-redwood-app directory
    Deleting old folder

--- hwa-redwood-app/web/src/pages/ArticlePage/ArticlePage.stories.jsx
+++
@@ -1,9 +0,0 @@
-import ArticlePage from './ArticlePage'
-
-const meta = {
-  component: ArticlePage,
-}
-
-export default meta
-
-export const Primary = {}
--- hwa-redwood-app/web/src/pages/ContactPage/ContactPage.stories.jsx
+++
@@ -1,9 +0,0 @@
-import ContactPage from './ContactPage'
-
-const meta = {
-  component: ContactPage,
-}
-
-export default meta
-
-export const Primary = {}
